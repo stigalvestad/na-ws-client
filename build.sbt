bd68b6949c6372118f3aafbfdc9efc042315cea6
Stig A <stigalvestad@gmail.com>
2015-11-10 04:42:55 +0100

    Improved config-reading using ficus

--- build.sbt
+++ build.sbt
@@ -6,8 +6,12 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
+resolvers ++= Seq(
+  "Sonatype OSS Releases"  at "http://oss.sonatype.org/content/repositories/releases/"
+)
 
 oneJarSettings
 
 libraryDependencies += "com.typesafe" % "config" % "1.3.0"
 libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.0.9"
+libraryDependencies += "net.ceedubs" %% "ficus" % "1.1.2"
--- src/main/scala/stig/no/config/ConfigApp.scala
+++ src/main/scala/stig/no/config/ConfigApp.scala
@@ -6,6 +6,9 @@ import java.util
 import com.typesafe.config.{Config, ConfigFactory}
 import org.slf4j.LoggerFactory
 
+import net.ceedubs.ficus.Ficus._
+import net.ceedubs.ficus.readers.ArbitraryTypeReader._
+
 object ConfigApp {
 
   def logger = LoggerFactory.getLogger(this.getClass)
@@ -21,17 +24,35 @@ object ConfigApp {
     else {
       val conf = ConfigFactory.parseFile(configFile)
 
-      conf.checkValid(ConfigFactory.defaultReference(), "nems-credentials", "tasks")
-      logger.info("The answer is: " + conf.getString("nems-credentials.username"))
+      conf.checkValid(ConfigFactory.defaultReference(), "nems", "tasks", "data_sources", "notifications")
 
-      val tasks = conf.getConfigList("tasks")
-      val iterator: util.Iterator[_ <: Config] = tasks.iterator()
-      while (iterator.hasNext){
-        val task = iterator.next()
-        val name = task.getString("name")
-        println(f"Task $name")
-      }
+      val nemsConfig = conf.as[NemsConfig]("nems")
+
+      val myNotifications = conf.as[MyNotifications]("notifications")
+
+      val myDataSources = conf.as[Set[MyDataSource]]("data_sources")
+
+      val myTasks = conf.as[List[MyTask]]("tasks")
+
+      val myConfig = MyConfig(nemsConfig, myNotifications, myDataSources, myTasks)
+
+      myConfig.tasks.foreach(println(_))
 
     }
   }
+
+  case class MyConfig(nemsConfig: NemsConfig,
+                      notifications: MyNotifications,
+                      data_sources: Set[MyDataSource],
+                      tasks: List[MyTask])
+
+  case class MyNotifications(recipients: Set[String], notify_level: String)
+
+  case class MyCredentials(username: String, password: String, api_key: String = "")
+
+  case class MyDataSource(name: String, category: String, credentials: MyCredentials)
+
+  case class MyTask(name: String, data_source_ref: String, tag_name: String, nems_data_type: String)
+
+  case class NemsConfig(target_url: String, credentials: MyCredentials)
 }
